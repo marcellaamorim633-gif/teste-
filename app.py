ffbcb5fe9aee1652a5542949d9be075395965fe6
marcellaamorim-18 <marcella.amorim@ssonic.com.br>
2026-06-04 17:22:25 -0300

    .

---
+++ app.py
@@ -0,0 +1,12 @@
+import html
+from flask import Flask, render_template
+app = Flask (__name__)
+
+@app.route("/")
+def hello ():
+    return "Hello word" 
+
+
+@app.route('/index')
+def index():
+    return render_template ('index.html')
